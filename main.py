d8b3980f382dd9fe2b0af602c8366605e7e7eb2a
Giovanni Prinzivalli <gprinziv@gmail.com>
2019-12-05 08:23:53 +0000

    Start of Day 3. Added test cases and changed main.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-return -1
+#Make a matrix and add 1 to any line that has a wire
+#When adding the second wire, check if that location == 2. If so, add it to the list of coordinates.
+#Find the MAnhattan Distance of each coordinate to the origin
+#select the shortest one.
+
+
+
+
+#Step 3, find the Manhattan Distance of each coordinate.
+def manhattan(x, y):
+  return abs(x) + abs(y)
