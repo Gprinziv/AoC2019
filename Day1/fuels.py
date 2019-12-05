904524a75db9a16b8e141cc37963aeb0cf27e8d5
Giovanni Prinzivalli <gprinziv@gmail.com>
2019-12-05 02:53:16 +0000

    Initial package. Day 1 complete. Still working on making individual packages work because lol.

---
+++ Day1/fuels.py
@@ -0,0 +1,19 @@
+import math
+
+def recurFuel(mass):
+  nextFuel = math.floor(int(mass)/3) - 2
+  return recurFuel(nextFuel) + nextFuel if nextFuel > 0 else 0
+
+def fuels(filename):
+  fuel = 0
+  modules = open(filename, "r")
+
+  for module in modules:
+    testFuel = recurFuel(int(module))
+    fuel += testFuel
+  
+  modules.close()
+  return fuel
+  
+def main():
+  print(fuels("list.txt"))
